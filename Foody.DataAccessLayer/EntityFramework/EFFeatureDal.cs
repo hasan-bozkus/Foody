86538e937867652ad7a32bdf397bcb0b09ab50d4
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-07-08 20:33:59 +0300

    Foody Project
    Chapter 10 - Entity Framework And Abstract

---
+++ Foody.DataAccessLayer/EntityFramework/EFFeatureDal.cs
@@ -0,0 +1,19 @@
+﻿using Foody.DataAccessLayer.Abstract;
+using Foody.DataAccessLayer.Context;
+using Foody.DataAccessLayer.Repositories;
+using Foody.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.EntityFramework
+{
+    public class EFFeatureDal : GenericRepository<Feature>, IFeatureDal
+    {
+        public EFFeatureDal(FoodyContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Foody.DataAccessLayer/EntityFramework/EFReviewDal.cs
@@ -0,0 +1,19 @@
+﻿using Foody.DataAccessLayer.Abstract;
+using Foody.DataAccessLayer.Context;
+using Foody.DataAccessLayer.Repositories;
+using Foody.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.EntityFramework
+{
+    public class EFReviewDal : GenericRepository<Review>, IReviewDal
+    {
+        public EFReviewDal(FoodyContext context) : base(context)
+        {
+        }
+    }
+}
