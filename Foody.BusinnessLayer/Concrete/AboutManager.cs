dcbf818b963ae0d0d38c50e09b2dfdbd20c0c875
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-07-09 10:53:35 +0300

    Foody Project
    Chapter 11 - Service Interface For Business Layer

---
+++ Foody.BusinnessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,38 @@
+﻿using Foody.BusinnessLayer.Abstract;
+using Foody.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.BusinnessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        public void TDelete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public About TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(About entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(About entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
