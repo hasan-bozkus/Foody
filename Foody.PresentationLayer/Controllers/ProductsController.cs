0c9604a028e2c0456bb508a05b22cce507feda64
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-09-02 09:15:59 +0300

    Foody Project
    Chapter 25 - Listing Categories in Dropdown Tool,
    Chapter 26 - Product Updates And Sidebar Icon Edits

--- Foody.PresentationLayer/Controllers/ProductsController.cs
+++ Foody.PresentationLayer/Controllers/ProductsController.cs
@@ -1,15 +1,19 @@
 ﻿using Foody.BusinnessLayer.Abstract;
+using Foody.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Foody.PresentationLayer.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public IActionResult ProductList()
@@ -19,7 +23,7 @@ namespace Foody.PresentationLayer.Controllers
         }
 
         public IActionResult ProductListWithCategory()
-        {
+        {            
             var values = _productService.TProductListWithCategory();
             return View(values);
         }
@@ -33,13 +37,32 @@ namespace Foody.PresentationLayer.Controllers
         [HttpGet]
         public IActionResult CreateProduct()
         {
+            var values = _categoryService.TGetAll();
+            ViewBag.categories = new SelectList(values, "CategoryId", "CategoryName");
             return View();
         }
 
         [HttpPost]
-        public IActionResult CreateProduct(int id)
+        public IActionResult CreateProduct(Product product)
         {
-            return View();
+            _productService.TInsert(product);
+            return RedirectToAction("ProductListWithCategory");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var values = _categoryService.TGetAll();
+            ViewBag.categories = new SelectList(values, "CategoryId", "CategoryName");
+            var productValues = _productService.TGetById(id);
+            return View(productValues);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _productService.TUpdate(product);
+            return RedirectToAction("ProductListWithCategory");
         }
     }
 }
